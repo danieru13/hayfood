5de06b5c6ba77eb1be01d3556c631c74b8deb345
Fabian Higuita <fabianes1996@gmail.com>
2021-10-14 12:43:41 -0500

    Update

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -25,27 +25,35 @@ class _MenuPageState extends State<MenuPage> {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10.0),
                   child: Container(
-                    child: Column(
+                    child: Column(                      
                       children: [
                         Container(
                           padding: EdgeInsets.only(left: 8.0),
                           child: headerText(
                               'Menu', Colors.black, FontWeight.bold, 30.0),
                         ),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1601314002592-b8734bca6604?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'),
+                        SizedBox(height: 10,),
                         menuItems(context,
                             'https://images.unsplash.com/photo-1610057099443-fde8c4d50f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80'),
+                        SizedBox(height: 10,),
                       ],
                     ),
                   ),
@@ -60,84 +68,94 @@ class _MenuPageState extends State<MenuPage> {
 }
 
 Widget menuItems(BuildContext context, String foto) {
-  return Column(
-    children: [
-      GestureDetector(
-          onTap: (){Navigator.pushNamed(context, "detail");},
-          child: Container(        
-          margin: EdgeInsets.only(left: 10),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Row(
-                  children: [
-                    ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image(
-                          fit: BoxFit.cover,
-                          height: 80.0,
-                          image: NetworkImage(foto),
-                          width: 80.0,
-                        )),
-                    Container(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              margin: EdgeInsets.symmetric(vertical: 7.0),
-                              child: headerText("Plato ejemplo", Colors.black,
-                                  FontWeight.bold, 17.0)),
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            margin: EdgeInsets.only(bottom: 5.0),
-                            child: Text(
-                              "Descripcion",
-                              style: TextStyle(
-                                  color: Colors.grey,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 13.0),
-                            ),
-                          ),
-                          Row(
+  return GestureDetector(
+      onTap: (){Navigator.pushNamed(context, "detail");},
+      child: Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+            BoxShadow(
+              color: Colors.black26, offset: Offset(2.0, 2.0), blurRadius: 8.0)
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        children: [
+          Container(        
+              margin: EdgeInsets.only(left: 10),
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image(
+                              fit: BoxFit.cover,
+                              height: 80.0,
+                              image: NetworkImage(foto),
+                              width: 80.0,
+                            )),
+                        Container(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                                size: 16.0,
-                              ),
-                              Text(
-                                '4.5',
-                                style: TextStyle(
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 13.0),
-                              ),
                               Container(
-                                margin: EdgeInsets.only(left: 5.0),
+                                  margin: EdgeInsets.symmetric(vertical: 7.0),
+                                  child: headerText("Plato ejemplo", Colors.black,
+                                      FontWeight.bold, 17.0)),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                margin: EdgeInsets.only(bottom: 5.0),
                                 child: Text(
-                                  "COP 20,000",
+                                  "Descripcion",
                                   style: TextStyle(
                                       color: Colors.grey,
                                       fontWeight: FontWeight.w500,
                                       fontSize: 13.0),
                                 ),
                               ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 16.0,
+                                  ),
+                                  Text(
+                                    '4.5',
+                                    style: TextStyle(
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 13.0),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 5.0),
+                                    child: Text(
+                                      "COP 20,000",
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 13.0),
+                                    ),
+                                  ),
+                                ],
+                              )
                             ],
-                          )
-                        ],
-                      ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),            
-            ],
-          ),
-        ),
-      )
-    ],
+                  ),            
+                ],
+              ),
+            ),        
+        ],
+      ),
+    ),
   );
 }
